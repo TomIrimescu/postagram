5e3af1b0b868260c8da856173ef47237679328e5
Tom Irimescu <tom@irimescu.com>
2020-07-16 18:55:04 -0400

    Add amplify, api, auth, storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import API from Amplify library
+import { API, Auth } from 'aws-amplify'
+
+// import query definition
+import { listPosts } from './graphql/queries'
+
+async function checkUser() {
+  const user = await Auth.currentAuthenticatedUser();
+  console.log('user:', user);
+  console.log('user attributes: ', user.attributes);
 }
 
-export default App;
+function App() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetchPosts();
+    checkUser(); // new function call
+  }, []);
+  async function fetchPosts() {
+    try {
+      const postData = await API.graphql({ query: listPosts });
+      setPosts(postData.data.listPosts.items)
+    } catch (err) {
+      console.log({ err })
+    }
+  }
+  return (
+    <div>
+      <h1>Hello Amplify</h1>
+      {
+        posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.name}</h3>
+            <p>{post.location}</p>
+            <p>{post.description}</p>
+          </div>
+        ))        
+      }
+      <AmplifySignOut />
+    </div>
+  )
+}
+
+export default withAuthenticator(App);
